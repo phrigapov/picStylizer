74a108a89c3043c81266cddfa28f87ea51448af4
lutian <lsalvino@hotmail.com>
2015-11-28 18:17:34 -0300

    update

--- example.php
+++ example.php
@@ -1,21 +1,8 @@
-# picStylizer
+<?php
 
-picStylizer is a PHP class that create sprite and css style file from images folder
+include('picStylizer.php');
 
-Notice: if you want to change the image onMouseOver (hover) you have add "_hover" at the end of filename before the extention
-Example: 
-// initial image
-icon.png
-// hover image
-icon_hover.png //adding _hover
-// the result css code will be:
-.icon {...}
-.icon:hover {...}
-
-# Usage
-
-```php
-// Initialize Class
+// initialize
 $pS = new picStylizer();
 
 // define folder configuration
@@ -34,13 +21,12 @@ $config = array(
 		);
 $pS->setFoldersConfig($config);
 
-// define minization (default: true)
+// define minization [true/false] (default: true)
 $pS->setMinization();
 
-// define css style by default
-$css = 'body {backgound-color:#000;font-family:courier;color:#fff,font-size:14px;}';
+// define css style by default (it will add to the sprites.css file)
+$css = 'body {background-color:#000;font-family:courier;color:#fff;font-size:14px;}';
 $pS->setCssInit($css);
 
 // gen sprites, styles and html example
 $pS->getSprite();
-```
